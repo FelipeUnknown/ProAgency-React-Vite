d38f02dd66dbadeb268ba5c1a39b674fc520a7cd
FelipeUnknown <felipedasilva1825@gmail.com>
2024-05-22 06:07:45 -0300

    terminar header mobile e fazer o resto do site

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,20 +1,42 @@
-import style from "../header/header.module.css"
-import Logo from "../../src/assets/img/Logo.svg"
+import { useState } from "react";
+import style from "../header/header.module.css";
+import Logo from "../../src/assets/img/Logo.svg";
+import iconBars from "../assets/img/iconBars.svg";
+import iconClose from "../assets/img/iconClose.svg";
 const Header =()=>{
+    const [menuActive, setMenuActive] = useState(false);
+    const links = [
+        {name: "Home", link: "/"},
+        {name: "Serviços", link: "/"},
+        {name: "Recurso", link: "/"},
+        {name: "Produto", link: "/"},
+        {name: "Depoimentos", link: "/"},
+        {name: "FAQ", link: "/"},        
+    ];
+    const toggleMenu = ()=>{
+        setMenuActive(! menuActive);
+    };
     return(
-        <header className={style.Header}>
+        <header className={`${style.Header} ${menuActive ? style.active : ""}`}>
             <div className={style.headerWrapper}>
                 <div className={style.headerLogo}>
                    <a href="/"><img src={Logo}/></a>
                 </div>
+                <button id={style.buttonMobile} onClick={toggleMenu}>
+                    <img 
+                    src={menuActive ? iconClose : iconBars} 
+                    id={style.menuIcons}
+                    alt="Icon Menu" />
+                </button>
                 <nav className={style.headerNav}>
-                    <ul className={style.navUl}>
-                        <li><a className={style.linkAH} href="/">Home</a></li>
-                        <li><a className={style.linkA} href="/">Serviços</a></li>
-                        <li><a className={style.linkA} href="/">Recurso</a></li>
-                        <li><a className={style.linkA} href="/">Produto</a></li>
-                        <li><a className={style.linkA} href="/">Depoimento</a></li>
-                        <li><a className={style.linkA} href="/">FAQ</a></li>
+                    <ul className={style.navUl} id={style.Menu}>
+                        {links.map((link, index)=>(
+                            <li className={style.navLi} key={link.name}>
+                                <a href={link.link} className={style.linkA} key={index}>
+                                    {link.name}
+                                </a>
+                            </li>
+                        ))}
                     </ul>
 
                 </nav>
